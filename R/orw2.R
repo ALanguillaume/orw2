834723aa31ed4f7ee9027d64b9da760498b7fe22
unknown <antoinelanguillaume@gmail.com>
2020-01-27 21:23:24 +0100

    bug fix: no answer + enter and different color correction

--- R/orw2.R
+++ R/orw2.R
@@ -5,47 +5,49 @@
 #' @export
 
 orw2 <- function(){
-  d = read.csv2(file = "./data/NL_onregelmatige_werkwoorden.csv")
-  d[c(1, 3)]
-  h = colnames(d)
-  a = "init"
-  while(a != "quit"){
 
+  # Read verb list
+  d = read.csv2(file = "./data/NL_onregelmatige_werkwoorden.csv")
+  # Define header
+  header = colnames(d)
+
+  answer = "init"
+  while(answer != "quit"){
+
+    # Sample one verb
     l = d[sample(1:nrow(d), 1), c(1:3)]
     l = as.character(unlist(l))
-    names(l) = h
+    names(l) = header
 
+    # Question text to display with hashed words
     disp = l
     disp[2:length(disp)] = gsub("[a-z]", "#", x = disp[2:length(disp)])
+    names(disp) = header
 
-    names(disp) = h
-
+    # Display question
     print(disp)
 
-    a = readline(prompt = "answer:")
+    # Take input from the user
+    answer = readline(prompt = "Answer:")
+    while(answer == "init" || answer == "") {
+      answer = readline(prompt = "Answer:")
+    }
 
-    if(a != "quit"){
-      a = unlist(strsplit(a, " "))
-      test <- a == l[2:length(l)]
-      correction <- l[2:length(l)]
+    # Process answer
+    answer = unlist(strsplit(answer, " "))
+    test <- answer == l[2:length(l)]
+    correction <- l[2:length(l)]
 
-      if(test == TRUE){
-        cat("Answer: ", crayon::green(test), "\n")
-      } else if(test == FALSE){
-        cat("Answer: ", crayon::red(test), "\n")
-      } else {
-        stop("something went wrong with the correctiong process")
-      }
-
+    if(length(answer) != 1 && answer != "quit"){
+      cat("Result: ", ifelse(test, crayon::green(test), crayon::red(test)), "\n")
       cat("Correction: ", correction, "\n")
       cat(paste(rep("-", 80), collapse = ""), "\n")
-    } else {
-      print("Tot ziens")
+
     }
   }
+  print("Tot ziens")
 }
 
-# To do if enter is pressed bug qrg length zero
-# differentiate TRUE / FALSE color
+
 # exception zullen "-"
 # avoid repeats of correct answers
